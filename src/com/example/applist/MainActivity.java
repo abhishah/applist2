f7e9f74d852c430b92a07ceddf21f5c93a521d8c
abhishah <abhishah501@gmail.com>
2014-07-14 11:23:02 +0530

    fixed bugs and added apk extractor

--- src/com/example/applist/MainActivity.java
+++ src/com/example/applist/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.applist;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
@@ -79,9 +88,10 @@ public class MainActivity extends ListActivity implements OnItemClickListener {
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		switch (item.getItemId()) {
-		case R.id.action_settings:
-			break;
 		case R.id.genapk:
+			new Gen().execute("data");
+			break;
+		case R.id.action_settings:
 			String a = getExtractPath();
 			Toast.makeText(getBaseContext(), a, Toast.LENGTH_LONG).show();
 			break;
@@ -97,27 +107,121 @@ public class MainActivity extends ListActivity implements OnItemClickListener {
 
 		return super.onMenuItemSelected(featureId, item);
 	}
+public class Gen extends AsyncTask<String,Integer,String>{
+	/*private void genapk() {
+		// TODO Auto-generated method stub
+		final Intent main=new Intent(Intent.ACTION_MAIN,null);
+		main.addCategory(Intent.CATEGORY_LAUNCHER);
+		final List packagelist=getPackageManager().queryIntentActivities(main, 0);
+		//int z=0;
+		for(Object object:packagelist){
+			ResolveInfo rs=(ResolveInfo)object;
+			File f1=new File(rs.activityInfo.applicationInfo.publicSourceDir);
+			Log.v("file--", " "+f1.getName().toString()+"----"+rs.loadLabel(getPackageManager()));
+			try{
+				String filename=rs.loadLabel(getPackageManager()).toString();
+				Log.d("file_name--",""+filename);
+				File f2;
+				String info=Environment.getExternalStorageState();
+                 if(info.equals(Environment.MEDIA_MOUNTED)){
+				 f2=new File(Environment.getExternalStorageDirectory().toString()+"/MyApps");}
+				else{ f2=getCacheDir();}
+				if(!f2.exists())f2.mkdirs();
+				f2=new File(f2.getPath()+"/"+filename+".apk");
+				f2.createNewFile();
+				InputStream in=new FileInputStream(f1);
+				OutputStream out=new FileOutputStream(f2);
+				byte[]bf=new byte[1024];
+				int len;
+				while((len=in.read(bf))>0){
+					out.write(bf,0,len);
+				}
+				in.close();
+				out.close();
+				System.out.println("File Copied");
+				
+			} catch(FileNotFoundException ex){
+                System.out.println(ex.getMessage() + " in the specified directory.");
+            }
+            catch(IOException e){
+                System.out.println(e.getMessage());      
+            }
+		}
+	}*/
+	ProgressDialog dialog;
+	
+	protected void onPreExecute() {
+		// example of setting up something
+		dialog = new ProgressDialog(MainActivity.this);
+		dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		dialog.setMax(100);
+		dialog.show();
 
-	/*
-	 * public void makeFile(List<PackageInfo> packageList1) { // TODO
-	 * Auto-generated method stubList<PackageInfo> packageList; String state;
-	 * StringBuilder data=new StringBuilder(""); packageList = packageList1;
-	 * state = Environment.getExternalStorageState(); if
-	 * (state.equals(Environment.MEDIA_MOUNTED)) { PackageInfo info; String
-	 * line=""; String l="\n"; for(int i=0;i<packageList.size();i++){
-	 * info=packageList.get(i); line=(String)
-	 * (getPackageManager().getApplicationLabel( info.applicationInfo));
-	 * data.append(line+l); } try { File myFile = new File(
-	 * Environment.getExternalStorageDirectory().getPath(),"apps.txt");
-	 * myFile.createNewFile(); FileOutputStream fOut = new
-	 * FileOutputStream(myFile); OutputStreamWriter myOutWriter = new
-	 * OutputStreamWriter(fOut); myOutWriter.append(data.toString());
-	 * myOutWriter.close(); fOut.close(); } catch (IOException e) { // TODO
-	 * Auto-generated catch block e.printStackTrace(); }
-	 * 
-	 * } }
-	 */
-
+	
+	}
+	@Override
+	protected String doInBackground(String... arg0) {
+		// TODO Auto-generated method stub
+	       
+		
+			// TODO Auto-generated method stub
+			final Intent main=new Intent(Intent.ACTION_MAIN,null);
+			main.addCategory(Intent.CATEGORY_LAUNCHER);
+			final List packagelist=getPackageManager().queryIntentActivities(main, 0);
+			int z=packageList.size();
+			double b=100/z;
+				for(final Object object:packagelist){
+				publishProgress((int)b);
+				Thread app=new Thread(new Runnable(){
+					public void run() {
+						ResolveInfo rs=(ResolveInfo)object;
+				if((rs.activityInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM)==0){
+				File f1=new File(rs.activityInfo.applicationInfo.publicSourceDir);
+				Log.v("file--", " "+f1.getName().toString()+"----"+rs.loadLabel(getPackageManager()));
+				try{
+					String filename=rs.loadLabel(getPackageManager()).toString();
+					Log.d("file_name--",""+filename);
+					File f2;
+					String info=Environment.getExternalStorageState();
+	                 if(info.equals(Environment.MEDIA_MOUNTED)){
+					 f2=new File(Environment.getExternalStorageDirectory().toString()+"/MyApps");}
+					else{ f2=getCacheDir();}
+					if(!f2.exists())f2.mkdirs();
+					f2=new File(f2.getPath()+"/"+filename+".apk");
+					f2.createNewFile();
+					InputStream in=new FileInputStream(f1);
+					OutputStream out=new FileOutputStream(f2);
+					byte[]bf=new byte[1024];
+					int len;
+					while((len=in.read(bf))>0){
+						out.write(bf,0,len);
+					}
+					in.close();
+					out.close();
+					System.out.println("File Copied");
+					
+				} catch(FileNotFoundException ex){
+	                System.out.println(ex.getMessage() + " in the specified directory.");
+	            }
+	            catch(IOException e){
+	                System.out.println(e.getMessage());      
+	            }
+			}
+			
+		};
+		});
+		app.start();
+		}
+	return null;
+	}
+	protected void onProgressUpdate(Integer... progress) {
+		dialog.incrementProgressBy(progress[0]);
+	}
+	protected void onPostExecute(String result) {
+		Toast.makeText(getApplicationContext(), "Back up of all application is made", Toast.LENGTH_LONG).show();
+	}
+	
+}
 	private String getExtractPath() {
 		// TODO Auto-generated method stub
 		return PreferenceManager.getDefaultSharedPreferences(this).getString(
@@ -130,8 +234,8 @@ public class MainActivity extends ListActivity implements OnItemClickListener {
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
-		MenuInflater mi=getMenuInflater();
-		mi.inflate(R.menu.context,menu);
+		MenuInflater mi = getMenuInflater();
+		mi.inflate(R.menu.context, menu);
 	}
 
 	@Override
